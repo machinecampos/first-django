8a03e0dbee39cec6f648ebf3822cd36acd18ec64
Eduardo Campos <edumachadocampos@gmail.com>
2024-05-11 23:13:00 -0300

    More code

--- web_project/urls.py
+++ web_project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] """
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import include, path
 
@@ -28,3 +29,5 @@ urlpatterns = [
     path("", include("hello.urls")),
     path('admin/', admin.site.urls)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
