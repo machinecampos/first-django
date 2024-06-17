f2e0c4dec8a6c30f9373acf6974d2f872312299c
Eduardo Campos <edumachadocampos@gmail.com>
2024-06-16 23:56:12 -0300

    More code

--- hello/urls.py
+++ hello/urls.py
@@ -4,4 +4,5 @@ from hello import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("hello/<name>", views.hello_there, name="hello_there"),
+    path("log/", views.log_message, name="log"),
 ]
